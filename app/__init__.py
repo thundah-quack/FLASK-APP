54783ea3fdfcb6b227827c807425bb53e61323a0
thundah-quack <thundahquack@gmail.com>
2024-07-07 11:49:57 -0400

    modified:   __pycache__/config.cpython-310.pyc 	modified:   app/__init__.py 	modified:   app/__pycache__/__init__.cpython-310.pyc 	modified:   app/__pycache__/microblog.cpython-310.pyc 	modified:   app/__pycache__/routes.cpython-310.pyc 	modified:   app/routes.py 	deleted:    app/templates/.base.swp 	modified:   app/templates/base.html 	modified:   app/templates/login.html 	modified:   config.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from config import Config
 
 app = Flask(__name__)
+
 app.config.from_object(Config)
 
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 from app import routes
+
